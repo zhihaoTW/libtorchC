dfc4df5ff96b6a3f43e122f60cfbd182aefcf140
zhihao chen <zhihao8841@gmail.com>
2024-08-30 11:37:00 +0800

    mnist

---
+++ mnist/build/CMakeFiles/mnist.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mnist.dir/mnist.cpp.o"
+  "CMakeFiles/mnist.dir/mnist.cpp.o.d"
+  "mnist"
+  "mnist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mnist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
