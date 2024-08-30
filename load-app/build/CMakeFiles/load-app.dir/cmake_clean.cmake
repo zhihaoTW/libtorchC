aac2c56a873a135436d300088e6edb9baf40872d
zhihao chen <zhihao8841@gmail.com>
2024-08-30 10:50:56 +0800

    load-app

---
+++ load-app/build/CMakeFiles/load-app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/load-app.dir/load-app.cpp.o"
+  "CMakeFiles/load-app.dir/load-app.cpp.o.d"
+  "load-app"
+  "load-app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/load-app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
