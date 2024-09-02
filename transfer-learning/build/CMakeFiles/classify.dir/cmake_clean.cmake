901ede95f5351e863fda65952ef358d2586fdeff
zhihao chen <zhihao8841@gmail.com>
2024-09-02 16:32:30 +0800

    transfer_learning

---
+++ transfer-learning/build/CMakeFiles/classify.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/classify.dir/classify.cpp.o"
+  "CMakeFiles/classify.dir/classify.cpp.o.d"
+  "classify"
+  "classify.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/classify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
